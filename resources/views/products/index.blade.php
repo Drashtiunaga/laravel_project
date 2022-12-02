dcd13cbc07e9741442118d9d8b44875bd42297f6
drashti <drashti.coderkube@gmail.com>
2022-12-02 17:25:16 +0530

    First commit from the second copy

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,51 @@
+@extends('products.layout')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <br><br>
+            <h2>Simple CRUD Operation</h2>
+        </div>
+        <div class="pull-right">
+            <br><br>
+            <a class="btn btn-success" href="{{ route('products.create') }}"> Create Product</a>
+        </div>
+    </div>
+</div>
+<br><br>
+
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
+
+<table class="table table-bordered data-table">
+    <tr>
+        <th>Sr.No</th>
+        <th>Name</th>
+        <th>Details</th>
+        <th width="300px;">Action</th>
+    </tr>
+    @php
+        $i= 1;
+    @endphp
+    @foreach ($products as $product)
+        <tr>
+            <td>{{ $i++ }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->detail }}</td>
+            <td>
+                <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a> 
+
+                <a class="btn btn-danger" href="{{ route('products.destroy',$product->id) }}">Delete</a> 
+            </td>
+        </tr>
+    @endforeach
+</table>
+{!! $products->links() !!}
+@endsection
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>crud operation in laravel 8</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+            <script src = "http://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content');
+        </div>
+    </body>
+</html>
